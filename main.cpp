5aaca4d33ad7581eee9f2a612d246715391c0d95
Simon Cornelius <simonjoecornelius@gmail.com>
2024-11-04 18:01:48 +0100

    magenta window yippie

--- main.cpp
+++ main.cpp
@@ -1,7 +1,74 @@
 #include <iostream>
+#include <windows.h>
 
-int main()
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    std::cout << "Hello, World!" << std::endl;
+    switch (uMsg)
+    {
+        case WM_PAINT: {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+
+            HBRUSH hBrush = CreateSolidBrush(RGB(200, 80, 255));
+            FillRect(hdc, &ps.rcPaint, hBrush);
+            DeleteObject(hBrush);
+
+            EndPaint(hwnd, &ps);
+            return 0;
+        }
+        case WM_DESTROY: {
+            PostQuitMessage(0);
+            return 0;
+        }
+        default: {
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
+        }
+    }
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    PSTR lpCmdLine, int nCmdShow)
+{
+
+    LPCSTR CLASS_NAME  = "Schublade Window Class";
+
+    WNDCLASS wc = {};
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        "Schublade",
+        WS_OVERLAPPEDWINDOW,
+
+        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    std::cout << "Created Window!" << std::endl;
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
 }
