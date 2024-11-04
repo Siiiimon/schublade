c034f1f4d7ee9d8f0aa3af871cada9087a302963
Simon Cornelius <simonjoecornelius@gmail.com>
2024-11-04 21:30:23 +0100

    text rendering
    also switched to D2D

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <windows.h>
+#include <d2d1.h>
+#include <dwrite.h>
 
-#define OPACITY 90
+#define OPACITY 100
 #define WIDTH_PERCENTAGE 0.8
 #define HEIGHT_PERCENTAGE 0.5
 #define DROPDOWN_DISTANCE_IN_PIXEL_PER_TICK 20
@@ -9,10 +11,20 @@
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+    static ID2D1Factory* d2d_factory = nullptr;
+    static IDWriteFactory* dwrite_factory = nullptr;
+    static ID2D1HwndRenderTarget* render_target = nullptr;
+
     static bool isAnimating = false;
 
     switch (uMsg)
     {
+        case WM_CREATE: {
+            CoInitialize(NULL);
+            D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &d2d_factory);
+            DWriteCreateFactory(DWRITE_FACTORY_TYPE_SHARED, __uuidof(IDWriteFactory), reinterpret_cast<IUnknown**>(&dwrite_factory));
+            return 0;
+        }
         case WM_KEYDOWN: {
             if (wParam == VK_ESCAPE) {
                 PostMessage(hwnd, WM_CLOSE, 0, 0);
@@ -54,16 +66,73 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         }
         case WM_PAINT: {
             PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hwnd, &ps);
+            BeginPaint(hwnd, &ps);
 
-            HBRUSH hBrush = CreateSolidBrush(RGB(34, 37, 48));
-            FillRect(hdc, &ps.rcPaint, hBrush);
-            DeleteObject(hBrush);
+            if (!render_target) {
+                RECT rc;
+                GetClientRect(hwnd, &rc);
+                D2D1_SIZE_U size = D2D1::SizeU(rc.right - rc.left, rc.bottom - rc.top);
+                d2d_factory->CreateHwndRenderTarget(
+                    D2D1::RenderTargetProperties(),
+                    D2D1::HwndRenderTargetProperties(hwnd, size),
+                    &render_target
+                );
+            }
 
+            render_target->BeginDraw();
+            render_target->Clear(D2D1::ColorF(0.11f, 0.09f, 0.15f));
+
+            ID2D1SolidColorBrush* brush = nullptr;
+            render_target->CreateSolidColorBrush(D2D1::ColorF(D2D1::ColorF::White), &brush);
+
+            IDWriteTextFormat* text_format = nullptr;
+            dwrite_factory->CreateTextFormat(
+                L"Agave Nerd Font",
+                nullptr,
+                DWRITE_FONT_WEIGHT_NORMAL,
+                DWRITE_FONT_STYLE_NORMAL,
+                DWRITE_FONT_STRETCH_NORMAL,
+                14.0f,
+                L"en-us",
+                &text_format
+            );
+
+            text_format->SetTextAlignment(DWRITE_TEXT_ALIGNMENT_CENTER);
+            text_format->SetParagraphAlignment(DWRITE_PARAGRAPH_ALIGNMENT_CENTER);
+
+            RECT rc;
+            GetClientRect(hwnd, &rc);
+            D2D1_RECT_F layout_rect = D2D1::RectF(static_cast<float>(rc.left), static_cast<float>(rc.top), static_cast<float>(rc.right), static_cast<float>(rc.bottom));
+
+            render_target->DrawText(
+                L"Welcome to the Terminal Emulator!",
+                wcslen(L"Welcome to the Terminal Emulator!"),
+                text_format,
+                layout_rect,
+                brush
+            );
+
+            render_target->EndDraw();
+
+            brush->Release();
+            text_format->Release();
             EndPaint(hwnd, &ps);
             return 0;
         }
         case WM_DESTROY: {
+            if (render_target) {
+                render_target->Release();
+                render_target = nullptr;
+            }
+            if (d2d_factory) {
+                d2d_factory->Release();
+                d2d_factory = nullptr;
+            }
+            if (dwrite_factory) {
+                dwrite_factory->Release();
+                dwrite_factory = nullptr;
+            }
+            CoUninitialize();
             PostQuitMessage(0);
             return 0;
         }
@@ -76,6 +145,8 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
     PSTR lpCmdLine, int nCmdShow)
 {
+    CoInitialize(NULL);
+
     LPCSTR CLASS_NAME  = "SchubladeWindowClass";
 
     WNDCLASS wc = {};
