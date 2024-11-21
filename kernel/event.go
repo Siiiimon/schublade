8480a2efa0095f14dec54d27686545dc7a8eb9ac
Simon Cornelius <simonjoecornelius@gmail.com>
2024-11-21 14:56:09 +0100

    basic FFI Event passing
    surely this is thread-safe right

---
+++ kernel/event.go
@@ -0,0 +1,55 @@
+package main
+
+/*
+#include <stdlib.h>
+typedef struct KernelEvent {
+	char* name;
+} KernelEvent;
+*/
+import "C"
+import "unsafe"
+
+var eventQueue = make(chan Event, 256)
+
+type Event struct {
+	name string
+}
+
+func PushEvent(e Event) {
+	select {
+	case eventQueue <- e:
+	default:
+		Log("Event", "failed to push %s. event queue length is %d\n", e.name, len(eventQueue))
+	}
+}
+
+func mallocEvent(inEvent Event) *C.KernelEvent {
+	cEvent := (*C.KernelEvent)(C.malloc(C.sizeof_KernelEvent))
+	if cEvent == nil {
+		panic("malloc failed")
+	}
+
+	cEvent.name = C.CString(inEvent.name)
+
+	return cEvent
+}
+
+//export FreeEvent
+func FreeEvent(event *C.KernelEvent) {
+	if event != nil {
+		if event.name != nil {
+			C.free(unsafe.Pointer(event.name))
+		}
+		C.free(unsafe.Pointer(event))
+	}
+}
+
+//export PollEvent
+func PollEvent() *C.KernelEvent {
+	select {
+	case event := <-eventQueue:
+		return mallocEvent(event)
+	default:
+		return nil
+	}
+}
